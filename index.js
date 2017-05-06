5a5c4683ebf429f25e40930f584925c4fff1b7a6
theDragonFire <bhbochikens@gmail.com>
2017-05-05 23:27:21 -0600

    Debugged so that it only works on #pingpong channel.

--- index.js
+++ index.js
@@ -6,23 +6,31 @@ console.log("Working...");
 var play = false;
 
 //Tell the toaster what to do
-toaster.on("message", (message) => {
+toaster.on("message", function(message) {
 
-    if((message.channel.id == "309908769163313152" && message.content == "Go Ahead 7719") || play) {
-        
-        if(message.content == "Go Ahead 7719") message.delete(0);
-        
-        play = true;
-        message.channel.sendMessage("PING :ping_pong:");
-        message.channel.sendMessage("PONG :ping_pong:");
-    };
+    if(message.channel.id == "309908769163313152") {
 
-    if(message.content.toUpperCase() == "STOP") {
-        message.delete(0);
-        play = false;
+        //If the phrase is right, start the game
+        if(message.content.toUpperCase() == "~GO") {
+            message.delete(0);
+            play = true;
+        }
+
+        //If the phrase is "STOP", stop the game
+        if(message.content.toUpperCase() == "~STOP") {
+            message.delete(0);
+            play = false;
+        }
+
+        //If the game is going, play
+        if(play) {
+            message.channel.sendMessage("PING :ping_pong:");
+            message.channel.sendMessage("PONG :ping_pong:");
+        }
+
     }
 
-})
+});
 
 //Log in Toaster
 toaster.login("MzA5NzgzNTQ5ODU3NDk3MTAx.C-0cig.4uz87eFSH1GXRWC5GWWxpXMGttE");
