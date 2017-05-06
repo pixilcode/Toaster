cfef8d294251c05a6c950976959be818b8be405d
theDragonFire <bhbochikens@gmail.com>
2017-05-05 23:38:08 -0600

    Finalized debugging, added #general channel commands section

--- index.js
+++ index.js
@@ -5,20 +5,23 @@ const toaster = new Discord.Client();
 console.log("Working...");
 var play = false;
 
+const begin = "$"
+
 //Tell the toaster what to do
 toaster.on("message", function(message) {
 
     if(message.channel.id == "309908769163313152") {
 
         //If the phrase is right, start the game
-        if(message.content.toUpperCase() == "~GO") {
+        if(message.content.toUpperCase() == begin + "GO") {
             message.delete(0);
             play = true;
         }
 
         //If the phrase is "STOP", stop the game
-        if(message.content.toUpperCase() == "~STOP") {
+        if(message.content.toUpperCase() == begin + "STOP") {
             message.delete(0);
+            message.channel.sendMessage("STOPPING");
             play = false;
         }
 
@@ -27,6 +30,8 @@ toaster.on("message", function(message) {
             message.channel.sendMessage("PING :ping_pong:");
             message.channel.sendMessage("PONG :ping_pong:");
         }
+
+    } else if(message.channel.id == "258349006520844288") {
 
     }
 
