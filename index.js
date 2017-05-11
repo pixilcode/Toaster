e40c6b5e082ec13d19932ce54497841a1b12098a
theDragonFire <bhbochikens@gmail.com>
2017-05-10 22:00:13 -0600

    Made a bunch of large changes that I don't even know.

--- index.js
+++ index.js
@@ -1,29 +1,41 @@
+//Declare functions
+function isCommand(str, message) {
+    if(message.author.bot) {
+        return false;
+    } else {
+        return message.content.startsWith(begin + str);
+    }
+}
+
 //Set up Discord and Toaster (the bot)
 const Discord = require("discord.js");
 const toaster = new Discord.Client();
 
-console.log("Working...");
 var play = false;
 
-const begin = "$";
+const begin = "~";
 
-const commands = [[begin + "go", "Start the game of ping-pong"], [begin + "stop", "Stop the game of ping-pong"], [begin + "roll [#]", "Roll a [#]-sided die"]];
+const commands = [[begin + "hello", "Say hello"], [begin + "go", "Start the game of ping-pong"], [begin + "stop", "Stop the game of ping-pong"], [begin + "roll [#]", "Roll a [#]-sided die"]];
+
+toaster.on("ready", function() {
+    console.log("Working...");
+});
 
 //Tell the toaster what to do
 toaster.on("message", function(message) {
 
+    var args = message.content.split(/[ ]+/);
+
     //#pingpong channel
-    if(message.channel.id == "309908769163313152") {
+    if(message.channel.name == "pingpong") {
 
         //If the phrase is "go", start the game
-        if(message.content == commands[0][0] && !(message.client.user.bot)) {
-            message.delete(0);
+        if(isCommand("go", message)) {
             play = true;
         }
 
         //If the phrase is "stop", stop the game
-        if(message.content == commands[1][0] && !(message.client.user.bot)) {
-            message.delete(0);
+        if(isCommand("stop", message)) {
             message.channel.sendMessage(":octagonal_sign: STOPPING :octagonal_sign:");
             play = false;
         }
@@ -34,23 +46,37 @@ toaster.on("message", function(message) {
             message.channel.sendMessage("PONG :ping_pong:");
         }
     //#general channel
-    } else if(message.channel.id == "258349006520844288") {
+    } else if(message.channel.name == "general") {
+
+        //If the phrase is "hello", respond
+        if(isCommand("hello", message)) {
+            message.channel.sendMessage("Hello, " + message.author.username);
+        }
+    
+    //#games
+    } else if(message.channel.name == "games") {
+        
+        //If the phrase is "hi", respond
+        if(isCommand("hi", message)) {
+            message.channel.sendMessage("HI!");
+        }
 
     }
 
     //Commands in all channels
     //If the phrase is "roll [#]", return a random number between 1 and the number
-    if(message.content.substring(0, 5) == commands[2][0].substring(0, 5)) {
-        var rand =  Math.ceil(Math.random() * Number(message.content.substring(6, message.content.length)));
+    if(isCommand("roll", message)) {
+        var rand =  Math.ceil(Math.random() * Number(args[1]));
         message.channel.sendMessage("" + rand);
     }
 
     //If the phrase is "help", display the help text
-    if(message.content == "$help") {
+    if(isCommand("help", message)) {
         for(var i = 0; i < commands.length; i++) {
             var commandDescription = commands[i][0] + "  :  " + commands[i][1];
             message.channel.sendMessage(commandDescription);
         }
+
     }
 
 });
